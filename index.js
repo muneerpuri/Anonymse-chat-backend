4b9eb53e7504ace5d9f1065b60c5465f04909808
Muneer Puri <muneer.p@tunicalabsmedia.com>
2022-03-13 01:35:38 +0530

    added express

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const io = require("socket.io")(process.env.PORT || 8900, {
-  cors: {
-    origin: "*",
-  },
-});
+const express = require("express")
+const socketIO = require("socket.io")
+const PORT = process.env.PORT || 8900;
+const INDEX = '/index.html';
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
 
 let users = [];
 
